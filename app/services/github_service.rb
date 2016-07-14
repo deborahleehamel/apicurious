9db4b671c41549ce99255ecda910f052734f7886
Deborah Hamel <deborahleehamel@gmail.com>
2016-07-14 07:11:49 -0600

    add organizations add orgs method to githubservice add self.orgs methos to githubprofile model update users show page to have Orgnanizations output

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -23,6 +23,10 @@ class GithubService
     get("/user/repos")
   end
 
+  def orgs
+    get("/users/#{current_user.username}/orgs")
+  end
+
   def get(path)
     JSON.parse(connection.get(path).body)
   end
